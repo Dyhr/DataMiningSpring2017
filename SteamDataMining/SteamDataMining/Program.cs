c168403d8ea31e9e82ae6c9f1139a055a82df11d
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2017-05-15 15:47:07 +0200

    Minor optimizations. Still really slow.

--- SteamDataMining/SteamDataMining/Program.cs
+++ SteamDataMining/SteamDataMining/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Globalization;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -29,74 +28,40 @@ namespace SteamDataMining
 
             // --------     SELF-ORGANIZING MAPS    ---------
 
-            //var mapSize = 64;
-            //var map = new Map(tags.Length, mapSize, data.Take(50).ToArray());
-            ////map.DumpCoordinates();
-            //var resultMap = map.ResultMap();
-
-            //Console.Write("Save SOM output as image? (Y/N):");
-            //var answer = Console.ReadLine();
-            //if (answer.ToLower() == "y")
-            //{
-
-            //}
-            //else
-            //{
-            //    var bitmap = new Bitmap(mapSize, mapSize);
-            //    var max = 0d;
-            //    for (int x = 0; x < mapSize; x++)
-            //    {
-            //        for (int y = 0; y < mapSize; y++)
-            //        {
-            //            var average = resultMap[x, y].Aggregate(0d, (acc, v) => acc + v) / tags.Length;
-            //            if (average > max) max = average;
-            //        }
-            //    }
-            //    for (int x = 0; x < mapSize; x++)
-            //    {
-            //        for (int y = 0; y < mapSize; y++)
-            //        {
-            //            var average = resultMap[x, y].Aggregate(0d, (acc, v) => acc + v) / max;
-            //            bitmap.SetPixel(x, y, Color.FromArgb((int)(average * 255), (int)(average * 255), (int)(average * 255)));
-            //        }
-            //    }
-            //    var box = new PictureBox();
-            //    box.Image = bitmap;
-            //}
-
-
-            // ---------    APRIORI MINING          ----------
-            var result = Apriori.MineItemSets(data.Select(x=>x.tags.Keys.ToArray()).ToList(), 0.03,3);
-
-
-            var resultMappedToRating = result.ToDictionary(r => SetAsString(r), r => getAverage(data.Where(x => r.All(x.tags.Keys.Contains))));//.Average(d => d.rank));
-
-            var asList = resultMappedToRating.ToList();
-            asList.Sort((kv, kv2) => kv.Value.CompareTo(kv2.Value));
-
-            WriteXML(asList.Select(a=>new ResultItem() {rating = a.Value,tags = a.Key.ToString()}).ToList(), @"c:\temp\test.xml");
-
-
-            //Console.WriteLine("Supported sets of length "+ result.First().Count+" found:");
-            foreach (var kv in asList)
-            {
-                Console.WriteLine(kv.Key + " : " + kv.Value);
+            var mapSize = 128;
+            var map = new Map(tags.Length, mapSize, 100, data);
+            //map.DumpCoordinates();
+            var resultMap = map.ResultMap();
+            var bitmap = new Bitmap(mapSize, mapSize);
+            var max = 0d;
+            for(int x = 0; x < mapSize; x++) {
+                for(int y = 0; y < mapSize; y++)
+                {
+                    var average = resultMap[x, y].Aggregate(0d, (acc, v) => acc + v);
+                    if (average > max) max = average;
+                }
+            }
+            for(int x = 0; x < mapSize; x++) {
+                for(int y = 0; y < mapSize; y++)
+                {
+                    var average = resultMap[x, y].Aggregate(0d, (acc, v) => acc + v) / max;
+                    bitmap.SetPixel(x, y, Color.FromArgb((int) (average*255), (int) (average*255), (int) (average*255)));
+                }
             }
 
+            Console.Write("Save SOM output as image? (Y/N): ");
+            var answer = Console.ReadLine();
+            if (answer.ToLower() == "y")
+            {
+                bitmap.Save("./SOM.png", ImageFormat.Png);
+            }
+            else
+            {
+                var box = new PictureBox();
+                box.Image = bitmap;
+                box.Show();
+            }
             Console.ReadLine();
-        }
-
-        private static string SetAsString(SortedSet<string> set)
-        {
-            var retString = "";
-
-            foreach (var item in set)
-            {
-                retString += (item + " ");
-            }
-            return retString;
-        }
-        
 
         private static void WriteXML(List<ResultItem> tagsWithRating, string filename)
         {
