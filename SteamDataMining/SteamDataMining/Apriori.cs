973892ab9cb99d1d1fc17ef0aeaae511d3b2ab95
Paw Høvsgaard <pawhovsgaard@gmail.com>
2017-05-15 14:16:08 +0200

    Using sorted list. Optimized running time by a lot

--- SteamDataMining/SteamDataMining/Apriori.cs
+++ SteamDataMining/SteamDataMining/Apriori.cs
@@ -9,22 +9,22 @@ namespace SteamDataMining
 {
     class Apriori
     {
-        public static List<HashSet<string>> MineItemSets(List<string[]> data, double supportThreshold, int minimumPrintSize)
+        public static List<SortedSet<string>> MineItemSets(List<string[]> data, double supportThreshold, int minimumPrintSize)
         {
             Console.WriteLine("Running Apriori using support threshold " + supportThreshold);
             int k;
-            List<HashSet<string>> supportedCandidates = new List<HashSet<string>>();
+            List<SortedSet<string>> supportedCandidates = new List<SortedSet<string>>();
 
             //double pct = 100 * supportThreshold / data.Count;
             int supportCount = (int) (supportThreshold * data.Count);
 
-            Dictionary<HashSet<string>, int> frequentItemSets = generateFrequentItemSetsLevel1(data, supportCount);
+            Dictionary<SortedSet<string>, int> frequentItemSets = generateFrequentItemSetsLevel1(data, supportCount);
 
             Console.WriteLine("Found " + frequentItemSets.Count + " supported length 1 patterns");
 
             for (k = 1; frequentItemSets.Count > 0; k++)
             {
-                supportedCandidates = new List<HashSet<string>>(frequentItemSets.Keys);
+                supportedCandidates = new List<SortedSet<string>>(frequentItemSets.Keys);
 
                 Console.WriteLine("Finding frequent itemsets of length " + (k + 1) + " ...");
                 frequentItemSets = generateFrequentItemSets(supportCount, data, frequentItemSets);
@@ -45,18 +45,18 @@ namespace SteamDataMining
             return supportedCandidates;
         }
 
-        private static Dictionary<HashSet<string>, int> generateFrequentItemSets(int supportThreshold, List<string[]> data, Dictionary<HashSet<string>, int> lowerLevelItemSets)
+        private static Dictionary<SortedSet<string>, int> generateFrequentItemSets(int supportThreshold, List<string[]> data, Dictionary<SortedSet<string>, int> lowerLevelItemSets)
         {
-            Dictionary<HashSet<string>, int> candidates = new Dictionary<HashSet<string>, int>();
+            Dictionary<SortedSet<string>, int> candidates = new Dictionary<SortedSet<string>, int>();
 
             // generate candidate itemsets from the lower level itemsets
-            foreach (HashSet<string> itemSet1 in lowerLevelItemSets.Keys)
+            foreach (SortedSet<string> itemSet1 in lowerLevelItemSets.Keys)
             {
-                foreach (HashSet<string> itemSet2 in lowerLevelItemSets.Keys)
+                foreach (SortedSet<string> itemSet2 in lowerLevelItemSets.Keys)
                 {
-                    if (!itemSet1.Equals(itemSet2))
+                    if (!itemSet1.Equals(itemSet2) && almostEqual(itemSet1,itemSet2))
                     {
-                        HashSet<string> newSet = joinSets(itemSet1, itemSet2);
+                        SortedSet<string> newSet = joinSets(itemSet1, itemSet2);
 
 
 
@@ -72,19 +72,33 @@ namespace SteamDataMining
             return candidates.ToDictionary(kv => kv.Key, kv => countSupport(kv.Key, data)).Where(c => c.Value >= supportThreshold).ToDictionary(kv => kv.Key, kv => kv.Value);
         }
 
-        private static HashSet<string> joinSets(HashSet<string> first, HashSet<string> second)
+        private static bool almostEqual(SortedSet<string> itemSet1, SortedSet<string> itemSet2)
         {
-            var items = new HashSet<string>();
+            var k = itemSet1.Count;
+
+            for (int i = 0; i < k-1; i++)
+            {
+                if (!itemSet1.ElementAt(i).Equals(itemSet2.ElementAt(i)))
+                    return false;
+            }
+            return true;
+        }
+
+        private static SortedSet<string> joinSets(SortedSet<string> first, SortedSet<string> second)
+        {
+            var items = new SortedSet<string>();
             foreach (var s in first)
                 items.Add(s);
             foreach (var s in second)
                 items.Add(s);
 
+            items.ToList();
+
             return items;
         }
 
         //return frequent 1-itemsets mapped to the support count
-        private static Dictionary<HashSet<string>, int> generateFrequentItemSetsLevel1(List<string[]> data, int supportThreshold)
+        private static Dictionary<SortedSet<string>, int> generateFrequentItemSetsLevel1(List<string[]> data, int supportThreshold)
         {
             Dictionary<string, int> returnTable = new Dictionary<string, int>();
 
@@ -104,12 +118,12 @@ namespace SteamDataMining
                 }
             }
 
-            return returnTable.Where(n => n.Value >= supportThreshold).ToDictionary(kv => new HashSet<string>() { kv.Key }, kv => kv.Value);
+            return returnTable.Where(n => n.Value >= supportThreshold).ToDictionary(kv => new SortedSet<string>() { kv.Key }, kv => kv.Value);
         }
 
         //returns how many tuples in our data set, that contains all items of the given set.
         // Assumes that items in ItemSets and transactions are both unique
-        private static int countSupport(HashSet<string> itemSet, List<string[]> data)
+        private static int countSupport(SortedSet<string> itemSet, List<string[]> data)
             => data.Where(item => itemSet.All(str => item.Contains(str))).Count();
 
     }
